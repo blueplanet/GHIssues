448cc4611478c23223095b3ddc944a01d388362c
erguolinge <erguolinge@gmail.com>
2012-01-04 07:23:06 +0900

    ﻿ViewModel追加

--- GHIssues/ViewModels/ReposViewModel.cs
+++ GHIssues/ViewModels/ReposViewModel.cs
@@ -11,7 +11,7 @@ using System.Windows.Shapes;
 
 namespace GHIssues.ViewModels
 {
-    public class ReposViewModel
+    public class ReposViewModel : ViewModel
     {
 
     }
---
+++ GHIssues/ViewModels/ViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Practices.Prism.ViewModel;
+
+namespace GHIssues.ViewModels
+{
+    public class ViewModel : NotificationObject
+    {
+        private bool isSynchronizing;
+        public bool IsSynchronizing
+        {
+            get { return this.isSynchronizing; }
+            set
+            {
+                if (this.isSynchronizing == value)
+                {
+                    return;
+                }
+
+                this.isSynchronizing = value;
+                this.RaisePropertyChanged(() => this.isSynchronizing);
+            }
+        }
+    }
+}
