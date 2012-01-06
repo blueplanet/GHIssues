b6d162eba2499851fb6622ba405ee41b91eca2ee
erguolinge <erguolinge@gmail.com>
2012-01-07 05:46:47 +0900

    ﻿テストプロジェクトを追加

---
+++ GHIssuesTests/ViewModel/ReposViewModelTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using GHIssues.Service.Models;
+using GHIssues.ViewModels;
+using Microsoft.Silverlight.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GHIssuesTests.ViewModel
+{
+    [TestClass]
+    public class ReposViewModelTest : SilverlightTest
+    {
+        [TestMethod]
+        [Asynchronous]
+        public void GetReposList()
+        {
+            ReposViewModel vm = new ReposViewModel();
+            vm.PropertyChanged += (s, e) =>
+            {
+                if (e.PropertyName != "Items")
+                {
+                    return;
+                }
+
+                Assert.IsTrue(vm.Items.Count > 0);
+                
+                Repository repo = vm.Items.Last();
+                Assert.IsNotNull(repo.name);
+                Assert.IsNotNull(repo.description);
+
+                EnqueueTestComplete();
+            };
+        }
+
+    } 
+}
